4d047d51815f00a449ecb862161d9144ec3eb12f
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-02-17 14:54:24 -0600

    Add files via upload

---
+++ break.js
@@ -0,0 +1,10 @@
+const rapperArray = ["kendrick", "j.cole", "mac", "leif"];
+
+
+for (let i = 0; i < rapperArray.length; i++){
+  if (i > 2) {
+    break;
+  }
+  console.log(rapperArray[i])
+}
+console.log("And if you don't know, now you know.");
