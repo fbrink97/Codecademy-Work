628efa6d6659f56777e1047733ce4900b2344911
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-03-02 16:48:23 -0600

    Add files via upload

---
+++ objectsproject1.js
@@ -0,0 +1,25 @@
+const meny = {
+    _courses: {
+      appetizers: [],
+      mains: [],
+      desserts: [],
+    },
+    get appetizers() {
+  
+    },
+    set appetizers(appetizerIn) {
+  
+    },
+    get mains() {
+  
+    },
+    set mains(mainsIn) {
+  
+    },
+    get desserts () {
+  
+    },
+    set desserts(dessertsIn) {
+  
+    }
+  };
