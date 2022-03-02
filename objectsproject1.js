dcbb9b1699fcb33009058f727416e392f161242a
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-03-02 17:02:00 -0600

    Update objectsproject1.js

--- objectsproject1.js
+++ objectsproject1.js
@@ -1,25 +1,32 @@
-const meny = {
-    _courses: {
-      appetizers: [],
-      mains: [],
-      desserts: [],
-    },
-    get appetizers() {
-  
-    },
-    set appetizers(appetizerIn) {
-  
-    },
-    get mains() {
-  
-    },
-    set mains(mainsIn) {
-  
-    },
-    get desserts () {
-  
-    },
-    set desserts(dessertsIn) {
-  
-    }
-  };
+const menu = {
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
+  get appetizers() {
+    return this._courses.appetizers;
+  },
+  get mains() {
+    return this._courses.mains;
+  },
+  get desserts () {
+    return this._courses.desserts;
+  },
+  set appetizers(appetizerIn) {
+    this._courses.appetizers - appetizers;
+  },
+  set mains(mainsIn) {
+    this._courses.mains - mains
+  },
+  set desserts(dessertsIn) {
+    this._courses.desserts - desserts;
+  },
+  get courses() {
+    return {
+      appetizers: this.appetizers, 
+      mains: this.mains,
+      desserts: this.desserts
+    };
+  },
+};
