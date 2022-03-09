4fb46cfb12e78e6ca8a2803f3a022c86b5fe941a
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-03-08 22:24:57 -0600

    Add files via upload

--- tempconversion.js
+++ tempconversion.js
@@ -1,8 +1,10 @@
 // creating constant for kelvin.
-const kelvin = 293;
+const kelvin = 273;
 // converting to celsius.
 let celsius = kelvin - 273;
 // converting from celsius to fahrenheit.
 let fahrenheit = celsius * (9/5) + 32;
 // rounds the conversion to fahrenheit from decimal to a whole number.
 fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
