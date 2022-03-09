0ebb545a47b82e2296cf4146398f8b5569d078c7
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-03-08 22:26:42 -0600

    Add files via upload

---
+++ tempconversionparttwo.js
@@ -0,0 +1,15 @@
+// creating constant for kelvin.
+const kelvin = 273;
+// converting to celsius.
+let celsius = kelvin - 273;
+// converting from celsius to fahrenheit.
+let fahrenheit = celsius * (9/5) + 32;
+// rounds the conversion to fahrenheit from decimal to a whole number.
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+let newton = celsius * (33/100);
+
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees in Newtons.`)
