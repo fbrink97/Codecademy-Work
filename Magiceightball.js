0f15c9ddb998c5ffdcad423e00dde98feb1b8619
fbrink97 <99011286+fbrink97@users.noreply.github.com>
2022-03-09 21:38:42 -0600

    Add files via upload

---
+++ Magiceightball.js
@@ -0,0 +1,9 @@
+// creating ternary expression for username
+let userName = '';
+
+userName ? console.log(`Hello, ${username}!`) : console.log('Hello!');
+
+// creating user question lines
+const userQuestion = 'Will I make enough money to pay all my debts?';
+
+console.log(`Magic 8 Ball, ${userName} would like to ask: ${userQuestion}`);
